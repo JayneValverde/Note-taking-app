cfd8ab8d30ab9dd92e9f911499106dcdd40a29a1
Jayne Valverde <jaynevalverde@gmail.com>
2023-07-08 14:02:25 -0500

    listening to PORT / GET request added

--- Develop/server.js
+++ Develop/server.js
@@ -11,3 +11,32 @@ const uuid = require('uuid');
 const app =  express();
 var PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// TODO: Static Middleware
+app.use(express.static('./Develop/public'));
+
+// TODO: "GET" request | Populate the saved notes from JSON file 
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/db/db.json'))
+});
+
+
+// TODO: "POST" request | Post new notes to the JSON file when entered and saved 
+
+
+
+// TODO: "DELETE" request | Bonus request 
+
+
+
+// TODO: HTML Routes
+
+
+
+// TODO: Listening 
+app.listen(PORT, () => 
+    console.log("App listening on PORT " + PORT)
+);
+
