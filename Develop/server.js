873077f3f6a89d5db4f0a3c25b1269dc4a106223
Jayne Valverde <jaynevalverde@gmail.com>
2023-07-10 15:14:48 -0500

    delted old notes from db.json w/out id's

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const notes = require('./db/db.json');
+// const notes = require('./db/db.json');
 
 // Helper method for generating unique id's
 const uuid = require('uuid');
